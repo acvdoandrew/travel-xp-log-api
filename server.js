977f96cc3fc85be4078182a1cc004027960d239d
zeuselijah <simeonhendricks@gmail.com>
2022-09-23 14:19:26 -0400

    wip: set up backend with dependencies, express iniatlization, connected to mongodb, mounted middleware and added app listener

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const cors = require('cors');
+
+// Initialize express app
+const app = express();
+// Configure application settings
+require('dotenv').config();
+
+const { PORT = 4000, DATABASE_URL } = process.env;
+// connect to mongodb
+mongoose.connect(DATABASE_URL);
+
+mongoose.connection
+  .on('connected', () => console.log('Connected to MongoDB'))
+  .on('disconnected', () => console.log('Disconnected from MongoDB'))
+  .on('error', (error) => console.log('Problem with MongoDB: ', error));
+
+// mount middleware
+app.use(express.json());
+// this middleware intercepts incoming json request bodies and turns them into req.body
+app.use(logger('dev'));
+app.use(cors());
+
+// mount router/controller
+
+// App Listen
+app.listen(PORT, () => {
+  console.log(`Express is listenting on port:${PORT}`);
+});
